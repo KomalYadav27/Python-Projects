c9520128105ad5ac1185a3c804df57eff8a0edff
Komal Yadav <113770567+KomalYadav27@users.noreply.github.com>
2023-03-19 21:25:23 +0530

    Add files via upload

---
+++ BMICalculator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # BMI Calculator
+# 
+# Referance: https://www.calculator.net/bmi-calculator.html
+# 
+
+# In[16]:
+
+
+weight = int((input("Enter your weight in kg:")))
+height = float((input("Enter your height in meters:")))
+BMI = weight/(height*height)
+print(BMI)
+if BMI>0:
+    if BMI<16:
+        print("You have severe thinness")
+    elif BMI<=17:
+        print("You have moderate thinness")
+    elif BMI<=18.5:
+        print("You have mild thinness")
+    elif BMI<=25:
+        print("You have normal weight")
+    elif BMI<=30:
+        print("You are overweight")
+    elif BMI<=35:
+        print("You are obese class 1")
+    elif BMI<=40:
+        print("You are obese class 2")
+    elif BMI>40:
+        print("You are obese class 3")
+    else:
+        print("Enter correct values of weight and height")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
